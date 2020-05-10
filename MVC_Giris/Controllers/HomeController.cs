a0bd59b647b1c2ebb7a2e8d1f20ae8f1930687d1
Gökay <48670495+gokay-urenc@users.noreply.github.com>
2020-05-10 04:12:32 +0300

    Initial commit

---
+++ MVC_Giris/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Giris.Controllers
+{
+    public class HomeController : Controller
+    {
+        public int Index()
+        {
+            return 2 + 2;
+        }
+
+        public string Mesaj()
+        {
+            return "Hello World.";
+        }
+    }
+}
+// Proje içerisinde birden fazla Controller olabilir.
+// Default olarak kullanıcıyı karşılayan controller, HomeController'dir.
+// Home, default tetiklenen method(action) index'tir.
+// Controller içinde birden fazla method barınabilir.
+// MVC'de kullanıcı isteğini web sayfası üzerinden link(url) ile yapar.
+// Yapılan istek MVC mantığında mutlaka bir controller'da bulunan methoda iletilmelidir.
+// Yani direkt olarak web sayfası adı kullanılarak görüntülenemez.
---
+++ README.md
@@ -0,0 +1,2 @@
+# ASP.NET-MVC-Intro
+ ASP.NET MVC giriş. / ASP.NET MVC intro
